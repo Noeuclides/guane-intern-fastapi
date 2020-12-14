f0b81e83ac3fd8f58a03eb5f87c8b67705d045ac
Noeuclides <euclidesnoeuclides@gmail.com>
2020-12-14 17:06:48 +0000

    setting database

--- database.py
+++ database.py
@@ -4,26 +4,15 @@ import urllib
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-# configure database
-host_server = os.environ.get('host_server', 'localhost')
-db_server_port = urllib.parse.quote_plus(
-    str(os.environ.get('db_server_port', '5432')))
-database_name = os.environ.get('database_name', 'guanedb1')
-db_username = urllib.parse.quote_plus(
-    str(os.environ.get('db_username', 'postgres')))
-print(db_username)
-db_password = urllib.parse.quote_plus(
-    str(os.environ.get('db_password', 'secret')))
-ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode', 'prefer')))
-DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(
-    db_username,
-    db_password,
-    host_server,
-    db_server_port,
-    database_name,
-    ssl_mode
-)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+print("*"*50)
+print("<<<>>>>>>", BASE_DIR)
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
 print(DATABASE_URL)
 
 metadata = sqlalchemy.MetaData()
